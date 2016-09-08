d05835cf1123a6ae18787a4917a94b989417d375
Artem Borzov <artem.borz@yandex.ru>
2016-09-08 22:01:16 +0500

    Initialize new project with MyGameBot

---
+++ GameBot/MyGameBot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SpurRoguelike.Core;
+using SpurRoguelike.Core.Primitives;
+using SpurRoguelike.Core.Views;
+
+namespace GameBot
+{
+    public class MyGameBot : IPlayerController
+    {
+        public Turn MakeTurn(LevelView levelView, IMessageReporter messageReporter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
